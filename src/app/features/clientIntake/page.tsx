a6a6150dbc897cb8a7a7e68c5f31c624e8d9b7f7
tstrum <trent.strum@gmail.com>
2024-11-24 10:00:04 -0800

    fahim's corrections to layout, global.css and tailwind.config

---
+++ src/app/features/clientIntake/page.tsx
@@ -0,0 +1,9 @@
+import ClientIntakeForm from './component/ClientIntakeForm';
+
+export default function Home() {
+	return (
+		<main className="min-h-screen bg-gray-100 py-12">
+			<ClientIntakeForm />
+		</main>
+	);
+}
